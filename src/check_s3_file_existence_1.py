132951aa487e8138e1c17ec8c99ab11d5fcfba56
Arunangsu Sahu <arunangsutech@gmail.com>
2023-10-05 18:13:25 +0530

    AWS s3 bucket file existence check

---
+++ src/check_s3_file_existence_1.py
@@ -0,0 +1,18 @@
+import boto3
+
+__author__ = "Jagannath"
+
+s3_client=boto3.client('s3')
+
+bucket_name='arunbucketsage'
+file_name='Person_details.csv'
+
+# check if the file exists in the bucket
+try:
+    file_content=s3_client.head_object(Bucket=bucket_name, Key=file_name)
+    print(f'File Exists:{file_name}')
+    print('****************************************')
+    print(f'File Content:\n{file_content}')    
+except FileExistsError as fe:
+    print('File Not Found:{fe}')    
+
